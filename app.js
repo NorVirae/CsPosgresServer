b8aff957504c71270ab46ac370405734a0af23c8
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-01-31 18:19:00 +0100

    set config file for db connections

--- app.js
+++ app.js
@@ -48,4 +48,5 @@ const port = process.env.PORT || 4000
 
 app.listen(port, ()=>{
     console.log("connection successful on port: "+port)
+    console.log(process.env.NODE_ENV)
 })
--- db/db.js
+++ db/db.js
@@ -1,19 +1,43 @@
 const Sequelize = require("sequelize")
+const DBConfig = require("../config")
 require('dotenv').config()
 
-module.exports = new Sequelize(database=process.env.DB_DATABASE, user=process.env.DB_USER, password=process.env.DB_PASSWORD, {
-    host:process.env.DB_HOST,
-    dialect:"postgres",
-    logging:false,
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        },
-    },
+console.log(DBConfig)
 
-    pool:{
-        max:5,
-        min:0,
-        idle:1000
+const SSLOption = (env) =>{
+    if(env === "production"){
+        return {
+            host:DBConfig.DB_HOST,
+            dialect:"postgres",
+            logging:false,
+            dialectOptions: {
+                
+                ssl: {
+                    rejectUnauthorized: false
+                },
+            },
+
+            pool:{
+                max:5,
+                min:0,
+                idle:1000
+            }
+                
+
     }
-})
+}else{
+    return{
+            host:DBConfig.DB_HOST,
+            dialect:"postgres",
+            logging:false,
+            pool:{
+                max:5,
+                min:0,
+                idle:1000
+            }
+
+    }
+ } 
+}
+
+module.exports = new Sequelize(database=DBConfig.DB_DATABASE, user=DBConfig.DB_USER, password=DBConfig.DB_PASSWORD, SSLOption(DBConfig.NODE_ENV))
